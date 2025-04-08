cc9eaba0b4152e38bc250070112c5fa1fc0f3d4c
Prachigarg24 <prachigarg858@gmail.com>
2025-04-08 14:04:01 +0530

    Refacterd PAI evaluation pushed

---
+++ uniqueEmailGenerator.js
@@ -0,0 +1,19 @@
+ const names = [
+    "John Smith",
+    "Jane Doe",
+    "John Simpson",
+    "Jane Davis"
+  ];
+  
+   const existingEmails = [
+    "johns@company.com",
+    "janed1@company.com"
+  ];
+
+  function generateEmails(names, existing){
+    const result = []
+    const emailSet = new Set(exisiting);
+    for(let fullName of names){
+        const [first , last]
+    }
+  }
